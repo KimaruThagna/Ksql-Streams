81f8e6cd58b27aa9ead94182c83df1936c02074d
Kimaru thagana <macbuntu@localhost.localdomain>
2021-06-07 14:21:39 +0300

    define streams and tables

--- mock_server_logs.py
+++ mock_server_logs.py
@@ -1,9 +1,6 @@
 from random import randint
 from datetime import datetime
-import json
+
 def generate_mock_logs(): 
     log_status = ["ERROR","SUCCESS","WARNING","HEALTHCHECK"]
-    return json.dumps({"date": f'[{datetime.now()}]',
-            "url": f'https://www.mockeserver.com/AP{randint(56786878,5678787878)}HIJ',
-            "status":f'{log_status[randint(0,3)]}'
-    })
+    return f'[{datetime.now()}]-https://www.mockeserver.com/AP{randint(56786878,5678787878)}HIJ STATUS {log_status[randint(0,3)]}'
