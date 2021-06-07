4aed0ddda81631ae269542b7c8c9c164d7299abc
Kimaru thagana <macbuntu@localhost.localdomain>
2021-06-07 13:51:18 +0300

    generate mock ratings for product

---
+++ mock_ratings.py
@@ -0,0 +1,10 @@
+from random import randint
+from faker import Faker
+def generate_mock_product_ratings(): 
+    faker = Faker()
+    return {
+        "userid": randint(1,2000),
+        "productid": randint(100,2000),
+        "description": faker.description(),
+        "rating": randint(1,5)
+    }
---
+++ mock_transactions.py
@@ -0,0 +1,15 @@
+from random import randint
+from datetime import datetime
+from faker import Faker
+
+def generate_mock_transactions(): 
+    faker = Faker()
+    action = ["DEPOSIT","WITHDRAWAL"]
+    return {
+        "action": action[randint(0,1)],
+        "user-id":f'fintech-{randint(1,10)}-UJ-{randint(10000,47999000)}',
+        "tx_id": f'TX-#{randint(10000000,90000000000)}',
+        "amount": randint(700000,10000000),
+        "timestamp": datetime.timestamp()
+        
+    }
