05a3bf4d8d6cd96d78e00cf3123afe55eda28fbe
Kimaru thagana <macbuntu@localhost.localdomain>
2021-06-07 16:42:01 +0300

    create different topics

--- common_producer.py
+++ common_producer.py
@@ -2,11 +2,16 @@ import os, time, json
 from kafka import KafkaProducer
 from random import choices, randint
 from string import ascii_letters, digits
+from .mock_ratings import generate_mock_product_ratings
+from .mock_server_logs import generate_mock_logs
+from .mock_transactions import generate_mock_transactions
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BROKER_URL")
 TRANSACTIONS_TOPIC = os.environ.get("TRANSACTIONS_TOPIC")
+RATINGS_TOPIC = os.environ.get("RATINGS_TOPIC")
+LOGS_TOPIC = os.environ.get("LOGS_TOPIC")
 
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL,
@@ -14,6 +19,10 @@ producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL,
                          value_serializer=lambda value: json.dumps(value).encode(),)
 
 while True:
-    transaction = create_random_transaction()
+    transaction = generate_mock_transactions()
+    rating = generate_mock_product_ratings()
+    log = generate_mock_logs()
     # Kafka messages are sent in bytes
     producer.send(TRANSACTIONS_TOPIC, value=transaction) 
+    producer.send(LOGS_TOPIC, value=log) 
+    producer.send(RATINGS_TOPIC, value=rating) 
